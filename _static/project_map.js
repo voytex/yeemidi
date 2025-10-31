55eeeef94908b1b2731cd4003392da2502fe3675
voytex <voytex@users.noreply.github.com>
2025-10-31 18:11:36 +0000

    deploy: 83b2db81338e03208fd8a1973cea8e2e765d3718

---
+++ _static/project_map.js
@@ -0,0 +1,23 @@
+// map of the project for the stable_uri forwarder
+const projectMap = {
+ "yeemidi/requirements/light_protocol.html": [
+
+
+  {"_LINK":"None-Light-Protocol" },
+
+  {"UID":"YMD-PROTO-1","_LINK":"YMD-PROTO-1" }, ],
+ "yeemidi/requirements/sys-reqs.html": [
+
+
+  {"_LINK":"None-System-Requirements" },
+
+  {"UID":"YMD-SYS-1","_LINK":"YMD-SYS-1" },
+
+  {"UID":"YMD-SYS-2","_LINK":"YMD-SYS-2" },
+
+  {"UID":"YMD-SYS-3","_LINK":"YMD-SYS-3" },
+
+  {"UID":"YMD-SYS-4","_LINK":"YMD-SYS-4" },
+
+  {"UID":"YMD-SYS-5","_LINK":"YMD-SYS-5" }, ],
+};
